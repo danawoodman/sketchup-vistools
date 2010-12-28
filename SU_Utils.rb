2d1da1d8347683596e1493796e9dab0f90dba13b
Dana Woodman <dana@danawoodman.com>
2010-12-28 15:04:16 -0800

    Adding two new tools: 'Freeze Groups and Components' and 'Unfreeze All' which allow lock and hide selected items, effectively freezing them.
    Updated the 'Show All' tool to ignore frozen and hidden groups or components, so it doesnt accidentally unfreeze things.
    
    Rearranged some code.

--- SU_Utils.rb
+++ SU_Utils.rb
@@ -18,37 +18,27 @@ class SU_Utils
     @entities = @model.entities
     @layers = @model.layers
   end
-  
-  # Hide selected layers.
-  # Grabs all layers in selection and turns their visibility off.
-  def hide_layers
-    selection_layers = @selection.collect { |s| s.layer }.to_a
-    if !@selection.empty?
-      @model.start_operation "Hide selected layers"
-          selection_layers.each { |l| l.visible = false }
-      @model.commit_operation    
-    end
-  end
 
   # Isolate selected layers.
   # Hide all layers that are not within the selection.
   def isolate_layers
     selection_layers = @selection.collect { |s| s.layer }.to_a
     layers_to_hide = @layers.to_a - selection_layers.to_a
-    if !@selection.empty?
+    if not @selection.empty?
       @model.start_operation "Isolate selected layers"
-          layers_to_hide.each { |l| l.visible = false }
+        layers_to_hide.each { |l| l.visible = false }
       @model.commit_operation    
     end
   end
-
-  # Hide selected entities.
-  # Hide all entities within the selection.
-  def hide_entities
-    if !@selection.empty?
-      @model.start_operation "Hide selected entities"
-          @selection.each { |e| e.visible = false }
-      @model.commit_operation    
+  
+  # Hide selected layers.
+  # Grabs all layers in selection and turns their visibility off.
+  def hide_layers
+    selection_layers = @selection.collect { |s| s.layer }.to_a
+    if not @selection.empty?
+      @model.start_operation "Hide selected layers"
+        selection_layers.each { |l| l.visible = false }
+      @model.commit_operation
     end
   end
 
@@ -57,18 +47,71 @@ class SU_Utils
   def isolate_entities
     if !@selection.empty?
       @model.start_operation "Isolate selected entities"
-          entities_to_hide = @entities.to_a - @selection.to_a
-          entities_to_hide.each { |e| e.visible = false }
+        entities_to_hide = @entities.to_a - @selection.to_a
+        entities_to_hide.each { |e| e.visible = false }
       @model.commit_operation    
     end
   end
 
+  # Hide selected entities.
+  # Hide all entities within the selection.
+  def hide_entities
+    if not @selection.empty?
+      @model.start_operation "Hide selected entities"
+        @selection.each { |e| e.visible = false }
+      @model.commit_operation    
+    end
+  end
+  
+  # Freeze selected entities.
+  # Hides and locks all groups and components within the selection. 
+  # Since lock only works on groups or components, this tool will only work 
+  # on groups and components.
+  def freeze_groups_and_components
+    if not @selection.empty?
+      @model.start_operation "Freeze groups and components"
+        @selection.each { |e| 
+          if e.typename == "Group" or e.typename == "ComponentInstance"
+            e.visible = false
+            e.locked = true
+          end
+        }
+      @model.commit_operation
+    end
+  end
+  
+  # Freeze selected entities.
+  # Hides and locks all groups and components within the selection. 
+  # Since lock only works on groups or components, this tool will only work 
+  # on groups and components.
+  def unfreeze_all
+    if not @selection.empty?
+      @model.start_operation "Unfreeze everything"
+        @entities.each { |e| 
+          if e.typename == "Group" or e.typename == "ComponentInstance"
+            if e.locked? and not e.visible?
+              e.locked = false
+              e.visible = true
+            end
+          end
+        }
+      @model.commit_operation
+    end
+  end
+
   # Show all
-  # Unhides all hidden layers and entities.
+  # Unhides all hidden layers and entities. If a layer is locked and hidden, 
+  # assume it is locked, so do not unhide it.
   def show_all
     @model.start_operation "Show all layers and entities"
       @layers.each { |l| l.visible = true }
-      @entities.each { |e| e.visible = true }
+      @entities.each { |e| 
+        if e.typename == "Group" or e.typename == "ComponentInstance" and e.locked?
+          e.visible = false
+        else
+          e.visible = true
+        end
+      }
     @model.commit_operation
   end
 
@@ -82,13 +125,11 @@ end
 # menus and the menu items for the tools.
 #
 # Only load these tools if the SU_Utils.rb file is not already loaded.
-if ( not file_loaded?("SU_Utils.rb") )
+if not file_loaded?("SU_Utils.rb")
   
   # ----------------------------------------------------------------------------
-  # Create the various commands
+  # Create the various Utility commands
   # ----------------------------------------------------------------------------
-  
-  # utils = SU_Utils.new()
   
   # Create the hide_layers command.
   hide_layers_cmd = UI::Command.new("Hide Layers") { 
@@ -134,6 +175,28 @@ if ( not file_loaded?("SU_Utils.rb") )
   isolate_entities_cmd.menu_text = isolate_entities_text
   isolate_entities_cmd.status_bar_text = isolate_entities_text
   
+  # Create the freeze_entities command.
+  freeze_groups_and_components_cmd = UI::Command.new("Freeze Groups and Components") { 
+    SU_Utils.new().freeze_groups_and_components()
+  }
+  freeze_groups_and_components_cmd.small_icon = "SU_Utils/images/freeze_groups_and_components_small.png"
+  freeze_groups_and_components_cmd.large_icon = "SU_Utils/images/freeze_groups_and_components_large.png"
+  freeze_groups_and_components_text = "Freeze groups and components"
+  freeze_groups_and_components_cmd.tooltip = freeze_groups_and_components_text
+  freeze_groups_and_components_cmd.menu_text = freeze_groups_and_components_text
+  freeze_groups_and_components_cmd.status_bar_text = freeze_groups_and_components_text
+  
+  # Create the unfreeze_all command.
+  unfreeze_all_cmd = UI::Command.new("Unfreeze All") { 
+    SU_Utils.new().unfreeze_all()
+  }
+  unfreeze_all_cmd.small_icon = "SU_Utils/images/unfreeze_all_small.png"
+  unfreeze_all_cmd.large_icon = "SU_Utils/images/unfreeze_all_large.png"
+  unfreeze_all_text = "Unfreeze all"
+  unfreeze_all_cmd.tooltip = unfreeze_all_text
+  unfreeze_all_cmd.menu_text = unfreeze_all_text
+  unfreeze_all_cmd.status_bar_text = unfreeze_all_text
+  
   # Create the hide_layers command.
   show_all_cmd = UI::Command.new("Show All") { 
     SU_Utils.new().show_all()
@@ -149,22 +212,26 @@ if ( not file_loaded?("SU_Utils.rb") )
   # Create and add the Utilities submenu.
   # ----------------------------------------------------------------------------
   utils_submenu = UI.menu("Plugins").add_submenu "Utilities"
-  utils_submenu.add_item hide_layers_cmd
   utils_submenu.add_item isolate_layers_cmd
-  utils_submenu.add_item hide_entities_cmd
+  utils_submenu.add_item hide_layers_cmd
   utils_submenu.add_item isolate_entities_cmd
+  utils_submenu.add_item hide_entities_cmd
+  utils_submenu.add_item freeze_groups_and_components_cmd
+  utils_submenu.add_item unfreeze_all_cmd
   utils_submenu.add_item show_all_cmd
   
   # ----------------------------------------------------------------------------
-  # Create and add the context menu shortcuts.
+  # Create and add the Utilities context menu shortcuts.
   # ----------------------------------------------------------------------------
   UI.add_context_menu_handler do |context_menu|
     context_menu.add_separator
     utils_context_submenu = context_menu.add_submenu "Utilities"
-    utils_context_submenu.add_item hide_layers_cmd
     utils_context_submenu.add_item isolate_layers_cmd
-    utils_context_submenu.add_item hide_entities_cmd
+    utils_context_submenu.add_item hide_layers_cmd
     utils_context_submenu.add_item isolate_entities_cmd
+    utils_context_submenu.add_item hide_entities_cmd
+    utils_context_submenu.add_item freeze_groups_and_components_cmd
+    utils_context_submenu.add_item unfreeze_all_cmd
     utils_context_submenu.add_item show_all_cmd
   end
 
@@ -172,10 +239,12 @@ if ( not file_loaded?("SU_Utils.rb") )
   # Create and add the Utilities toolbar.
   # ----------------------------------------------------------------------------
   utils_toolbar = UI::Toolbar.new("Utilities")
-  utils_toolbar.add_item hide_layers_cmd
   utils_toolbar.add_item isolate_layers_cmd
-  utils_toolbar.add_item hide_entities_cmd
+  utils_toolbar.add_item hide_layers_cmd
   utils_toolbar.add_item isolate_entities_cmd
+  utils_toolbar.add_item hide_entities_cmd
+  utils_toolbar.add_item freeze_groups_and_components_cmd
+  utils_toolbar.add_item unfreeze_all_cmd
   utils_toolbar.add_item show_all_cmd
   utils_toolbar.show
 
